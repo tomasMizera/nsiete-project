d708d627d7d9c5a3416c9f745d20f7ed627676f6
Matej Čief <matej.cief@gmail.com>
2019-11-16 16:48:34 +0100

    First neural network

---
+++ project/src/data/generator.py
@@ -0,0 +1,199 @@
+import numpy as np
+import cv2
+import keras
+
+def np_resize(img, input_shape):
+    """
+    Reshape a numpy array, which is input_shape=(height, width), 
+    as opposed to input_shape=(width, height) for cv2
+    """
+    height, width = input_shape
+    return cv2.resize(img, (width, height))
+
+
+def mask2rle(img):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+
+def rle2mask(rle, input_shape):
+    width, height = input_shape[:2]
+
+    mask = np.zeros(width*height).astype(np.uint8)
+
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]
+    lengths = array[1::2]
+
+    current_position = 0
+    for index, start in enumerate(starts):
+        mask[int(start):int(start+lengths[index])] = 1
+        current_position += lengths[index]
+
+    return mask.reshape(height, width).T
+
+
+def build_masks(rles, input_shape, reshape=None):
+    depth = len(rles)
+    if reshape is None:
+        masks = np.zeros((*input_shape, depth))
+    else:
+        masks = np.zeros((*reshape, depth))
+
+    for i, rle in enumerate(rles):
+        if type(rle) is str:
+            if reshape is None:
+                masks[:, :, i] = rle2mask(rle, input_shape)
+            else:
+                mask = rle2mask(rle, input_shape)
+                reshaped_mask = np_resize(mask, reshape)
+                masks[:, :, i] = reshaped_mask
+
+    return masks
+
+
+def build_rles(masks, reshape=None):
+    width, height, depth = masks.shape
+
+    rles = []
+
+    for i in range(depth):
+        mask = masks[:, :, i]
+
+        if reshape:
+            mask = mask.astype(np.float32)
+            mask = np_resize(mask, reshape).astype(np.int64)
+
+        rle = mask2rle(mask)
+        rles.append(rle)
+
+    return rles
+
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+
+    def __init__(self, list_IDs, df, target_df=None, mode='fit',
+                 base_path='../../data/train_images',
+                 batch_size=32, dim=(1400, 2100), n_channels=3, reshape=None,
+                 n_classes=4, random_state=2019, shuffle=True):
+        self.dim = dim
+        self.batch_size = batch_size
+        self.df = df
+        self.mode = mode
+        self.base_path = base_path
+        self.target_df = target_df
+        self.list_IDs = list_IDs
+        self.reshape = reshape
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_batch = [self.list_IDs[k] for k in indexes]
+
+        X = self.__generate_X(list_IDs_batch)
+
+        if self.mode == 'fit':
+            y = self.__generate_y(list_IDs_batch)
+            return X, y
+
+        elif self.mode == 'predict':
+            return X
+
+        else:
+            raise AttributeError(
+                'The mode parameter should be set to "fit" or "predict".')
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.seed(self.random_state)
+            np.random.shuffle(self.indexes)
+
+    def __generate_X(self, list_IDs_batch):
+        'Generates data containing batch_size samples'
+        # Initialization
+        if self.reshape is None:
+            X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        else:
+            X = np.empty((self.batch_size, *self.reshape, self.n_channels))
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            img_path = f"{self.base_path}/{im_name}"
+
+            if self.n_channels == 3:
+                img = self.__load_rgb(img_path)
+            else:
+                img = self.__load_grayscale(img_path)
+
+            if self.reshape is not None:
+                img = np_resize(img, self.reshape)
+
+            if len(img.shape) == 2:
+                img = np.expand_dims(img, axis=-1)
+
+            # Store samples
+            X[i, ] = img
+
+        return X
+
+    def __generate_y(self, list_IDs_batch):
+        if self.reshape is None:
+            y = np.empty((self.batch_size, *self.dim,
+                          self.n_classes), dtype=int)
+        else:
+            y = np.empty((self.batch_size, *self.reshape,
+                          self.n_classes), dtype=int)
+
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            image_df = self.target_df[self.target_df['ImageId'] == im_name]
+
+            rles = image_df['EncodedPixels'].values
+
+            if self.reshape is not None:
+                masks = build_masks(
+                    rles, input_shape=self.dim, reshape=self.reshape)
+            else:
+                masks = build_masks(rles, input_shape=self.dim)
+
+            y[i, ] = masks
+
+        return y
+
+    def __load_grayscale(self, img_path):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = img.astype(np.float32) / 255.
+        img = np.expand_dims(img, axis=-1)
+
+        return img
+
+    def __load_rgb(self, img_path):
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img.astype(np.float32) / 255.
+
+        return img
