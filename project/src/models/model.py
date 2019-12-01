ff96789c567754d2fd6275393e8a066c1e6d4d51
tomasMizera <tomas.mizera2@gmail.com>
2019-12-01 17:06:21 +0100

    model refactor + model summary

---
+++ project/src/models/model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import keras
+import cv2
+import os
+from sklearn.model_selection import train_test_split
+from segmentation_models import Unet
+from segmentation_models import get_preprocessing
+from ..data.generator import DataGenerator
+from util import dice_coef
+
+train = pd.read_csv('../data/train.csv')
+train['ImageId'] = train['Image_Label'].apply(lambda x: x.split('_')[0])
+train['cat'] = train['Image_Label'].apply(lambda x: x.split('_')[1])
+train['has_mask'] = ~pd.isna(train['EncodedPixels'])
+train['missing'] = pd.isna(train['EncodedPixels'])
+train_nan = train.groupby('ImageId').agg('sum')
+train_nan.columns = ['No: of Masks', 'Missing masks']
+
+mask_count_df = pd.DataFrame(train_nan)
+mask_count_df = train.groupby('ImageId').agg(np.sum).reset_index()
+mask_count_df.sort_values('has_mask', ascending=False, inplace=True)
+
+BATCH_SIZE = 32
+
+train_idx, val_idx = train_test_split(
+    mask_count_df.index, random_state=2019, test_size=0.15
+)
+
+train_generator = DataGenerator(
+    train_idx,
+    df=mask_count_df,
+    target_df=train,
+    batch_size=BATCH_SIZE,
+    reshape=(256, 384),
+    n_channels=3,
+    n_classes=4
+)
+
+val_generator = DataGenerator(
+    val_idx,
+    df=mask_count_df,
+    target_df=train,
+    batch_size=BATCH_SIZE,
+    reshape=(256, 384),
+    n_channels=3,
+    n_classes=4
+)
+
+
+def create_model():
+    preprocess = get_preprocessing('resnet34')  # for resnet, img = (img-110.0)/1.0
+    model = Unet('resnet34', input_shape=(256, 384, 3),
+             classes=4, activation='sigmoid')
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=[dice_coef])
+    return model
+
+
+model = create_model()
+
